6d6f1e3d12191a84d9e096ae4b37a540e365baf8
Nikhil Gehlot <111073969+gehlotnikhil@users.noreply.github.com>
2024-08-03 02:55:55 +0000

    login bug solved

--- js/log.js
+++ js/log.js
@@ -1,9 +1,15 @@
-function log()
-{
-    a = document.f1.first.value;
-   
-    sessionStorage.setItem("Name", a);
-    window.location.href ="mymovie.html";
-
-
+function log() {
+  a = document.f1.first.value;
+  b = document.f1.last.value;
+  c = document.f1.username.value;
+  d = document.f1.email.value;
+  e = document.f1.country.value;
+  f = document.f1.zip.value;
+    if(a=="" || b=="" ||c==""||d==""||e=="")
+        return;
+    else{
+        
+  sessionStorage.setItem("Name", a);
+  window.location.href = "mymovie.html";
+    }
 }
